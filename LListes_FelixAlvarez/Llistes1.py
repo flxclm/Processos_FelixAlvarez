ce1b164cce079de62f1b32bc4338375790ffbb51
flxclm <felix8ao@gmail.com>
2019-10-01 19:53:45 +0200

    LLista random

---
+++ LListes_FelixAlvarez/Llistes1.py
@@ -0,0 +1,4 @@
+import random
+
+llista = [random.randint(0,100), random.randint(0,100), random.randint(0,100), random.randint(0,100), random.randint(0,100), random.randint(0,100), random.randint(0,100), random.randint(0,100), random.randint(0,100), random.randint(0,100)]
+print(llista)
