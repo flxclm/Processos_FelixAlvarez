22d6b2f3123b13896904054228794ba22b732841
flxclm <felix8ao@gmail.com>
2019-10-02 19:34:00 +0200

    Equacions primer grau

---
+++ Practica2_Equacio/Equacions.py
@@ -0,0 +1,29 @@
+class EquacioPrimerGrau:
+
+    def __init__(self,equacio):
+        self.seq = equacio
+        self.a=" "
+        self.b=" "
+        self.operador=" "
+        self.c= " "
+
+    def calcula(self):
+
+        equ=self.seq.split()
+        a=equ[0]
+        a=a[1-1]
+        b=equ[2]
+        operador=equ[1]
+        c=equ[4]
+
+        if(operador=="+"):
+            resultat=(int(c)-int(b))/int(a)
+
+            if(operador=="-"):
+                resultat=(int(c)+int(b))/int(a)
+
+            print("x = "+str(resultat))
+
+equacio = EquacioPrimerGrau("2x + 3 = 7")
+print(equacio.seq)
+equacio.calcula()
