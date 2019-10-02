51ae094ea97f0d0a97e4d7549dfd8082215c4b5e
flxclm <felix8ao@gmail.com>
2019-10-02 19:51:48 +0200

    Equacions2

---
+++ Practica2_Equacio/Equacions2.py
@@ -0,0 +1,8 @@
+from Equacions import EquacioPrimerGrau
+
+eq = EquacioPrimerGrau("20x + 30 = 70")
+eq.calcula()
+eq = EquacioPrimerGrau("20x - 70 = -30")
+eq.calcula()
+eq = EquacioPrimerGrau("40x + 80 = 52")
+eq.calcula()
