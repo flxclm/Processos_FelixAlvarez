6aac7dedd28b19a4640ebccd4d0760fd21cae070
flxclm <56040561+flxclm@users.noreply.github.com>
2019-10-23 17:07:40 +0200

    Add files via upload

--- BOT DBA/DBA.py
+++ BOT DBA/DBA.py
@@ -5,14 +5,36 @@ import telebot
 
 bot = telebot.TeleBot("924183682:AAERg7f-Y9djeyEylF7WJT5wJ_SYjx9pBV8")
 
-@bot.message_handler(commands=['help'])
+@bot.message_handler(commands=["help"])
 def send_welcome(message):
-	bot.reply_to(message, "1 Saluda al bot con un hola")
-        bot.reply_to(message, "2 Selecciona la opcion correcta")
-        bot.reply_to(message, "3 Sé muy cuidadoso con lo que dices!")
+	bot.reply_to(message, "1 Saluda al bot amb un /hola")
+        bot.reply_to(message, "2 Selecciona la opció correcta.")
+        bot.reply_to(message, "3 Vigila la teva resposta! El bot és molt sensible.")
+
+def resposta(a,b,c,d):
+
+        
+
+@bot.message_handler(commands=["hola"])
+def send_welcome(message):
+    bot.reply_to(message, "Oh, hola...")
+
+    if resposta == a
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-	bot.reply_to(message, message.text)
-
-bot.polling()
+	bot.reply_to(message, message.text)bot.polling()
