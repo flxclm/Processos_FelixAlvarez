12f83b9193c187c07b805ac5965a2b82d425c541
flxclm <56040561+flxclm@users.noreply.github.com>
2019-10-23 18:11:19 +0200

    Add files via upload

--- BOT DBA/DBA.py
+++ BOT DBA/DBA.py
@@ -7,19 +7,32 @@ bot = telebot.TeleBot("924183682:AAERg7f-Y9djeyEylF7WJT5wJ_SYjx9pBV8")
 
 @bot.message_handler(commands=["help"])
 def send_welcome(message):
-	bot.reply_to(message, "1 Saluda al bot amb un /hola")
-        bot.reply_to(message, "2 Selecciona la opció correcta.")
-        bot.reply_to(message, "3 Vigila la teva resposta! El bot és molt sensible.")
+	bot.reply_to(message, "1· Saluda al bot amb un /hola \n 2· Selecciona la opció correcta. \n 3· Vigila la teva resposta! El bot és molt sensible. ")
 
-def resposta(a,b,c,d):
-
-        
 
 @bot.message_handler(commands=["hola"])
 def send_welcome(message):
-    bot.reply_to(message, "Oh, hola...")
+    bot.reply_to(message, "Oh, hola... \n a) Què fas aquí? \n b)Qui ets tú? \n c) No sembles molt content. \n d) No diguis res.")
 
-    if resposta == a
+@bot.message_handler(regexp="a")
+def resposta(message):
+
+        bot.send_message(message.chat.id, "Espero.")
+
+@bot.message_handler(regexp="b")
+def resposta(message):
+
+        bot.send_message(message.chat.id, "És de mala educació preguntar el nom d'algú sense presentar-te abans.")
+
+@bot.message_handler(regexp="c")
+def resposta(message):
+
+        bot.send_message(message.chat.id, "Ho hauría d'estar?")
+
+@bot.message_handler(regexp="d")
+def resposta(message):
+
+        bot.send_message(message.chat.id, "(El bot espera una resposta)")
 
 
 
@@ -29,12 +42,4 @@ def send_welcome(message):
 
 
 
-
-
-
-
-
-
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-	bot.reply_to(message, message.text)bot.polling()
+bot.polling()
